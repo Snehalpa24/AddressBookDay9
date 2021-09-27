07161f3305c7a0dfc9c90787b8437411095192de
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-09-28 00:00:38 +0530

    Address book Uc2 is added

---
+++ AddressBookUc2Day9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookUc2Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC2_AddNewContact.AddPerson();
+        }
+    }
+}
