906781b2fccd852a21f172fd59ca5aa37fea0c82
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-09-28 00:02:03 +0530

    Address book Uc4 is added

---
+++ AddressBookUc4Day9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookUc4Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC4_DeleteContact.RemovePerson();
+        } 
+    }
+}
