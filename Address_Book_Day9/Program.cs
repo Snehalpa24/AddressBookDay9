f7bb30fa93e717d991e7dfec597f534513b31921
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-09-27 23:59:30 +0530

    AddressBook Uc1 is added

---
+++ Address_Book_Day9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Address_Book_Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1_CreatContact_Program.ListPeople();
+            UC1_CreatContact_Program.PrintPerson(Person person)();
+            
+
+        }
+    }
+}
